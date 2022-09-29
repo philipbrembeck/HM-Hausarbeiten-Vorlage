96a5cb98c4a71ef2efa561ca3e4b6d76d22f6b80
Philip <4144601+philipbrembeck@users.noreply.github.com>
2022-09-30 00:24:10 +0200

    Update index_2.php

--- webservice/index_2.php
+++ webservice/index_2.php
@@ -57,13 +57,19 @@ $i18n->init();
           <div class="form-group-body">
             <textarea id="appendix" name="appendix" cols="40" rows="4" class="form-control"></textarea>
             <span id="textHelpBlock" class="form-text color-fg-subtle"><?php echo L::index2_form_eg; ?> <span class="latex">L<sup>A</sup>T<sub>E</sub>X</span> Code <code>\subsection{Unterpunkt}</code></span><br>
-            <button name="submit" type="submit" class="btn btn-primary mb-3 mt-3"><?php echo L::index2_form_submit; ?></button>
+            <button name="submit" type="submit" class="btn btn-primary mb-3 mt-3" id="btn"><?php echo L::index2_form_submit; ?></button>
           </div>
         </div>
       </form>
       <p class="color-fg-muted">&copy; 20<?php echo date('y'); ?> <a href="//philipbrembeck.com">Philip Brembeck</a> - Licensed under <a href="https://github.com/philipbrembeck/HM-Hausarbeiten-Vorlage/blob/main/LICENSE">WTFPL</a> &middot; All data collected is immediately deleted after submitting the form.</p>
 
        <script>
+        const btn = document.getElementById('btn');
+        btn.addEventListener('click', function handleClick() {
+          const initialText = '<span>Loading</span><span class="AnimatedEllipsis"></span>';
+          btn.innerHTML = `<span>Loading</span><span class="AnimatedEllipsis"></span>`;
+        });
+
          const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
          if (prefersDarkScheme.matches) {
            document.documentElement.setAttribute('data-color-mode', 'dark');
@@ -72,6 +78,11 @@ $i18n->init();
            document.documentElement.removeAttribute('data-color-mode', 'dark');
            document.documentElement.removeAttribute('data-dark-theme', 'dark_dimmed');
          }
+
+         const btn = document.getElementsByClassName('btn');
+         btn.addEventListener('click', function handleClick() {
+          btn.textContent = '<span>Loading</span><span class="AnimatedEllipsis"></span>';
+        });
       </script>
    </body>
 </html>
